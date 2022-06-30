61ba67cf8022709b9c7a106d9885ab6e0f6be9f1
Bogdan <bogdan_kolesov@bk.ru>
2022-06-30 22:02:34 +0600

    start

---
+++ app/screens/intro.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { View, StyleSheet, Text, TextInput } from 'react-native';
+
+const Intro = () => {
+    return (
+        <View>
+            <Text>
+                Enter Your name to continue
+            </Text>
+            <TextInput />
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({})
+
+export default Intro;
