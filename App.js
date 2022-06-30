b46523dd9e1b19fd2bd3783a00673069ee7e7b4a
Bogdan <bogdan_kolesov@bk.ru>
2022-06-30 23:13:30 +0600

    fix

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import Intro from './app/screens/intro';
+import Intro from './app/screens/Intro';
 
 export default function App() {
   return (
