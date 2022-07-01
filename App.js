e501896ebc64b0ddaf69ac434e073a20071fc413
Bogdan <bogdan_kolesov@bk.ru>
2022-07-01 19:58:57 +0600

    fix

--- App.js
+++ App.js
@@ -6,18 +6,19 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import NoteScreen from './src/screens/NoteScreen';
 
 export default function App() {
-  const [user, setUser] = useState();
+  const [user, setUser] = useState({});
 
   const findUser = async () => {
     const result = await AsyncStorage.getItem('user')
-    console.log(result)
-    setUser(JSON.parse(result))
+    // console.log(result)
+    if (result !== null) {
+      setUser(JSON.parse(result))
+    }
   }
   useEffect(() => {
     findUser()
   }, [])
-  return (
-    <NoteScreen user={user} />
-  );
+  if (!user.name) return <Intro />
+  return <NoteScreen user={user} />
 }
 
